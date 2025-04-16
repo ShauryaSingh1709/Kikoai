f161b341f34df44de0f96194c95dc89832c6a20e
Shaurya Singh <evolvexmindset@gmail.com>
2025-04-16 17:17:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ GEMINI_API_KEY = "AIzaSyC_LICEcED-w3wuk--p1s_8yqyFuYQLH5g"
 
 # ElevenLabs API Key (for text-to-speech)
 ELEVENLABS_API_KEY = "sk_f6bc50703251e63d2885eb1de688d64bd9063dce850aaf14"
+VOICE_ID = "21m00Tcm4TlvDq8ikWAM" 
 
 # MongoDB URI (for memory storage)
 MONGO_URI = "mongodb+srv://Lelouch:Lelouch123@cluster0.vmf8l.mongodb.net/?retryWrites=true&w=majority"
