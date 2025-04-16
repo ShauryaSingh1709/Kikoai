bd547f879edf5e11382a1822482cf0f606d6d747
ADiBariya <adiiibariya@gmail.com>
2025-04-16 18:31:10 +0530

    Update assets.py

--- gui/assets.py
+++ gui/assets.py
@@ -1,7 +1,15 @@
 import os
 
-def get_avatar_path():
-    return os.path.join("assets", "avatar", "default.gif")
+def get_avatar_path(name="default"):
+    """
+    Returns the path to the avatar image based on the given name.
+    If not found, returns the path to the default avatar.
+    """
+    base_path = os.path.join(os.path.dirname(__file__), "avatars")
+    avatar_file = f"{name}.png"
+    full_path = os.path.join(base_path, avatar_file)
 
-def get_sound_path(name):
-    return os.path.join("assets", "sounds", f"{name}.mp3")
+    if not os.path.exists(full_path):
+        return os.path.join(base_path, "default.png")
+
+    return full_path
